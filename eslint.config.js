623d323724956dc14be5bc3231b06b8d2cf00952
yuvan <yuvan162007@gmail.com>
2026-08-27 20:38:01 +0530

    lint: add ESLint flat config with React and hooks plugins

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+﻿import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'build/'],
+  },
+];
